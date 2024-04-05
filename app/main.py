365e16fd10842329a159dd7dccd543f556345976
Abhijit2714 <abijitdhamne@gmail.com>
2024-04-05 11:13:20 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"message": "Hello, Infivit!!!!"}
+    return {"message": "Hello, Hope is a good thing may be the best of things and no good things ever dies!!!! Good Morning!!!!"}
